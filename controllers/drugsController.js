50d48ea4e7308cbbc1ba1c979b3baa15d2abe416
Yasser <yassergeo@gmail.com>
2021-02-26 15:06:29 -0500

    update controllers

--- controllers/drugsController.js
+++ controllers/drugsController.js
@@ -25,7 +25,46 @@ const updateDrug = async (req, res) => {
   }
 };
 
+const findDrug = async (req, res) => {
+  try {
+    const drug = await Drug.findById(req.params.id);
+    res.json(drug);
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+
+const findOneDrugAndUpdate = async (req, res) => {
+  try {
+    const drug = await Drug.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        name: req.body.name,
+        typeDrug: req.body.typeDrug,
+        amount: req.body.amount,
+        price: req.body.price,
+        localization: req.body.localization,
+      }
+    );
+    res.json(drug);
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+
+const deleteDrug = async (req, res) => {
+  try {
+    const removeDrug = await Drug.remove({ _id: req.params.id });
+    res.json(removeDrug);
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+
 module.exports = {
   drugsList,
   updateDrug,
+  findDrug,
+  findOneDrugAndUpdate,
+  deleteDrug,
 };
--- routes/drugs.js
+++ routes/drugs.js
@@ -1,47 +1,22 @@
 //const { json } = require("body-parser");
 const express = require("express");
 const router = express.Router();
-const Drug = require("../models/Drug");
-const { drugsList, updateDrug } = require("./../controllers/drugsController");
+const {
+  drugsList,
+  updateDrug,
+  findDrug,
+  findOneDrugAndUpdate,
+  deleteDrug,
+} = require("./../controllers/drugsController");
 
 router.post("/", updateDrug);
 
 router.get("/", drugsList);
 
-router.get("/:id", async (req, res) => {
-  try {
-    const drug = await Drug.findById(req.params.id);
-    res.json(drug);
-  } catch (error) {
-    res.json({ message: error });
-  }
-});
+router.get("/:id", findDrug);
 
-router.patch("/:id", async (req, res) => {
-  try {
-    const drug = await Drug.findOneAndUpdate(
-      { _id: req.params.id },
-      {
-        name: req.body.name,
-        typeDrug: req.body.typeDrug,
-        amount: req.body.amount,
-        price: req.body.price,
-        localization: req.body.localization,
-      }
-    );
-    res.json(drug);
-  } catch (error) {
-    res.json({ message: error });
-  }
-});
+router.patch("/:id", findOneDrugAndUpdate);
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const removeDrug = await Drug.remove({ _id: req.params.id });
-    res.json(removeDrug);
-  } catch (error) {
-    res.json({ message: error });
-  }
-});
+router.delete("/:id", deleteDrug);
 
 module.exports = router;
