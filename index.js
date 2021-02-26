be1b7e2bc41b007eb771b0c5a6111fda5c6afabf
Yasser <yassergeo@gmail.com>
2021-02-26 12:56:25 -0500

    update readme file

--- index.js
+++ index.js
@@ -14,20 +14,7 @@ mongoose.connect(process.env.MONGO_DB_STRING, {
 
 app.use("/", homeRoute);
 app.use("/drugs", drugRoute);
-//Simular un sistema de inventario de farmacia.
 
-/* POST->Registrar un medicamento
-o Nombre
-o Tipo de medicamento
-o Cantidad
-o Fecha de registro
-o Precio
-o Ubicación
-GET->Traer la lista de medicamentos guardados
-GET->Traer un elemento por id o nombre
-PUT->Editar un documento de la API (por ID)
-DELETE-> Eliminar un documento de la API (por ID)
-Podrá utilizar SQLITE, MONGODB, O OBJETOS en memoria para almacenar los datos de la API */
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
